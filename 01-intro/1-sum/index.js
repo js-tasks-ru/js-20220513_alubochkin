1bfbf8fb3340cd7a097253351b5ed5c83265f94f
Александр <43494847+alubochkin@users.noreply.github.com>
2022-05-13 22:23:41 +0300

    sum done

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -1,9 +1,17 @@
 /**
  * sum
- * @param {number} m base
- * @param {number} n index
- * @returns {number}
+ * @param number m base
+ * @param number n index
+ * @returns number || Error
  */
 export default function sum(m, n) {
+  m = parseInt(m);
+  n = parseInt(n);
+  if (isNaN(m) || isNaN(n)) {
+    throw new Error(`parameters cannot be reduced to a number`);
+  }
 
+  if (typeof m === "number" && typeof n === "number") {
+    return Number(m) + Number(n);
+  }
 }
