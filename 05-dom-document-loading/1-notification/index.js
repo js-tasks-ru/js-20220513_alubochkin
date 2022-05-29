6a176db39bf5c423fe960f0115484c61ec3a4ecd
Александр <43494847+alubochkin@users.noreply.github.com>
2022-05-29 22:19:57 +0300

    sortable

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,74 @@
 export default class NotificationMessage {
+  $root = document.body;
+  idTimeOut = null;
+  attr = null;
+  element = null;
 
+  constructor(message = '', { duration = 0, type = 'success' } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.rootTagCreate();
+  }
+
+  rootTagCreate() {
+    //       <div ${this.attr} class="notification success" style="--value:${value}s">
+    const value = this.duration / 1000;
+    this.attr = 'data-element="notification"';
+
+    const div = document.createElement('div');
+    div.classList.add('notification', this.type);
+    div.setAttribute('style', `--value:${value}s`);
+    div.setAttribute('data-element', 'notification');
+
+    div.innerHTML = this.template;
+
+    this.element = div;
+  }
+
+  get template() {
+    return `
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+    `;
+  }
+
+  timer() {
+    return setTimeout(() => {
+      this.remove();
+      this.attr = null;
+    }, this.duration);
+  }
+
+  show(el = this.$root) {
+    this.remove();
+
+    el.insertAdjacentElement('beforeend', this.element);
+    this.idTimeOut = this.timer();
+  }
+
+  remove() {
+    const notification = this.$root.querySelector(
+      '[data-element="notification"]'
+    );
+
+    if (notification) {
+      notification.remove();
+    }
+    if (this.idTimeOut) {
+      clearTimeout(this.idTimeOut);
+    }
+  }
+
+  destroy() {
+    this.remove();
+    // this.$root = null;
+    this.element = null;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,120 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  element = document.createElement('div');
+  subElements = {
+    root: document.createElement('div'),
+    headers: document.createElement('div'),
+    body: document.createElement('div'),
+  };
+  fieldValue = null;
+  orderValue = null;
 
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.render();
+  }
+
+  sort(fieldValue, orderValue) {
+    console.log(fieldValue, orderValue);
+
+    const sortableFn = (a, b) => {
+      const collator = new Intl.Collator('ru', {
+        sensitivity: 'case',
+        caseFirst: 'upper',
+        numeric: true,
+      });
+      a = a[fieldValue];
+      b = b[fieldValue];
+      if (orderValue === 'desc') {
+        return collator.compare(b, a);
+      }
+      return collator.compare(a, b);
+    };
+    this.data.sort(sortableFn);
+    this.render();
+  }
+
+  get templateHeader() {
+    // return this.headerConfig.map(({ id, title, sortable, sortType }) => {});
+
+    return `
+    <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="">
+        <span>Image</span>
+    </div>
+    <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="">
+      <span>Name</span>
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+      </span>
+      </div>
+      <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="">
+        <span>Quantity</span>
+      </div>
+      <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="">
+        <span>Price</span>
+      </div>
+      <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="">
+        <span>Sales</span>
+      </div>
+    `;
+  }
+
+  get tempateProducts() {
+    return this.data
+      .map(({ images = [], title, quantity, price, sales, id }) => {
+        return `
+          <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
+            <div class="sortable-table__cell">
+              <img 
+                class="sortable-table-image" 
+                alt="Image" 
+                src="https://news-it.ru/images/default.jpg">
+            </div>
+            <div class="sortable-table__cell">${title}</div>
+
+            <div class="sortable-table__cell">${quantity}</div>
+            <div class="sortable-table__cell">${price}</div>
+            <div class="sortable-table__cell">${sales}</div>
+          </a>
+      `;
+      })
+      .join('');
+  }
+
+  clear() {
+    for (const el in this.subElements) {
+      this.subElements[el].innerHTML = '';
+    }
+  }
+
+  roottemplate() {
+    const { headers, body, root } = this.subElements;
+    this.clear();
+
+    body.classList.add('sortable-table__body');
+    body.insertAdjacentHTML('afterbegin', this.tempateProducts);
+
+    headers.classList.add('sortable-table__header', 'sortable-table__row');
+    headers.insertAdjacentHTML('afterbegin', this.templateHeader);
+
+    root.classList.add('products-list__container');
+    root.insertAdjacentElement('afterbegin', headers);
+
+    root.insertAdjacentElement('beforeend', body);
+  }
+
+  render() {
+    this.roottemplate();
+    this.element.classList.add('sortable-table');
+    this.element.innerHTML = '';
+
+    console.log('this.element', this.element);
+
+    this.element.appendChild(this.subElements.root);
+  }
+
+  destroy() {
+    this.element = null;
+    return null;
   }
 }
-
